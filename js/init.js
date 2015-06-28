5fda8bd57782260e880fbaebb93ca7c78a722112
Lane Goodman <goodman.ls@gmail.com>
2015-06-28 14:10:48 -0400

    piechart2

--- js/init.js
+++ js/init.js
@@ -1,14 +1,3 @@
-jQuery(window).scroll(function() {
-    if (scroll >= 50) {
-        $('#logo-image').attr('src', 'img/Acme_Monogram_Colour.png')
-        $(".important-class").addClass("padding-on-my-header");
-    }
-    if (scroll < 50) {
-        $(".important-class").removeClass("padding-on-my-header");
-        $('#logo-image').attr('src', 'img/Acme_Colour.png')
-    }
-});
-
 /* 
 ===============================================================
 jQuery plugin to expand/collapse a content element when a 
