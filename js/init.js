753e0c23d65820a2990a01493058037a38aa9dbd
Lane Goodman <goodman.ls@gmail.com>
2015-07-08 16:26:44 -0400

    Slick Attempt

--- js/init.js
+++ js/init.js
@@ -6,6 +6,53 @@ $(document).on("scroll",function(){
     }
 });
 
+ var $windowWidth=$(window).width();
+
+$( window ).resize(function() {
+var $windowWidth=$(window).width();
+if(($windowWidth > 1) && ($windowWidth < 640)){
+$('.slick-multislider').unslick();  
+      $('.slick-multislider').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          dots: true,
+        });
+      
+    } else {
+$('.slick-multislider').unslick();      
+        $('.slick-multislider').slick({
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          dots: true,
+        });
+    }   
+
+});  
+
+if(($windowWidth > 1) && ($windowWidth < 640)){
+
+      $('.slick-multislider').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          dots: true,
+        });
+      
+    } else {
+        $('.slick-multislider').slick({
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          dots: true,
+        });
+    }
+    
 /* 
 ===============================================================
 jQuery plugin to expand/collapse a content element when a 
