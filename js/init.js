bd092461474ef3acbebd5ec5dc47523722fc6daa
Lane Goodman <goodman.ls@gmail.com>
2015-09-25 17:05:00 -0400

    removing lint

--- js/init.js
+++ js/init.js
@@ -399,51 +399,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
  var $windowWidth=$(window).width();
-/**
-$( window ).resize(function() {
-var $windowWidth=$(window).width();
-if(($windowWidth > 1) && ($windowWidth < 640)){
-$('.slick-multislider').unslick();  
-      $('.slick-multislider').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 2000,
-          dots: true,
-        });
-      
-    } else {
-$('.slick-multislider').unslick();      
-        $('.slick-multislider').slick({
-          slidesToShow: 3,
-          slidesToScroll: 2,
-          autoplay: true,
-          autoplaySpeed: 2000,
-          dots: true,
-        });
-    }   
 
-});  
-
-if(($windowWidth > 1) && ($windowWidth < 640)){
-
-      $('.slick-multislider').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 2000,
-          dots: true,
-        });
-      
-    } else {
-        $('.slick-multislider').slick({
-          slidesToShow: 3,
-          slidesToScroll: 2,
-          autoplay: true,
-          autoplaySpeed: 2000,
-          dots: true,
-        });
-    }
 
 /***Expand***/
 
